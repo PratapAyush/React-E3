414a3d3e748b9c81e784828d6cc0519e0f8bef03
PratapAyush <ayushpratapsingh235@gmail.com>
2022-06-06 17:05:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       {
         // Code here
+        <Navbar/>
+
       }
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { CartContext } from "../../context/CartContext";
 
 // use react-router Link or NavLink
-const Link = <a />;
+const Link = <a href="/">Home</a>;
+
+<NavLink to="/react">
+  React
+</NavLink>
 
 const Navbar = () => {
+
+  const { isAuth, logout } = useContext(AuthContext);
+  const { ItemsCount } = useContext(CartContext);
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    // login screen
+    if (isAuth) {
+      logout();
+      // he wants to logout
+    } else {
+      // he wants to login
+      navigate("/login");
+    }
+  };
   return (
     <div data-cy="navbar">
-      <Link data-cy="navbar-home-link"></Link>
-      <span data-cy="navbar-cart-items-count">{/* count here */}</span>
-      <button data-cy="navbar-login-logout-button"></button>
+      <Link data-cy="navbar-home-link">Log</Link>
+      <span data-cy="navbar-cart-items-count">
+        {/* count here */} Cart: {ItemsCount && `(${cartItemsCount})`}
+        </span>
+      <button data-cy="navbar-login-logout-button" onClick={handleLoginClick}>
+      {isAuth ? "Logout" : "Login"}
+      </button>
     </div>
   );
 };
